fd4c4c3306a22b5404904c2e3356e43dbcb44b98
ZiaUrR3hman <ziaurr3hman@hotmail.com>
2015-08-12 21:18:09 +0500

    Update V 14.0 ... Fixed Like and Unlike Issue....

--- FacebookAutoLikeUnlimited.meta.js
+++ FacebookAutoLikeUnlimited.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
 // @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeUnlimited.user.js
-// @version  		13.0
+// @version  		14.0
 // ==/UserScript==
