b84e0f29371fe1fbde7bbd115a5fd5efc2cd3795
ZiaUrR3hman <ziaurr3hman@hotmail.com>
2015-11-19 05:33:05 +0500

    Introducing  Facebook Auto Comment.. Fixed Theme issue and added random time interval b/w each like..

--- FacebookAutoLikeUnlimited.meta.js
+++ FacebookAutoLikeUnlimited.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
 // @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeUnlimited.user.js
-// @version  		14.0
+// @version  		15.0
 // ==/UserScript==
