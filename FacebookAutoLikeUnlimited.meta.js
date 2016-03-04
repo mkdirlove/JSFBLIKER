e65e9d08c011825ca02b933a0a8e982d9da5e6a0
Zia Ur Rehman (Z.R.F) <ziaurr3hman@hotmail.com>
2016-03-04 19:30:47 +0500

    Fixed Auto Reply issue on Facebook Pages...

--- FacebookAutoLikeUnlimited.meta.js
+++ FacebookAutoLikeUnlimited.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
 // @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeUnlimited.user.js
-// @version  		18.0
+// @version  		19.0
 // ==/UserScript==
