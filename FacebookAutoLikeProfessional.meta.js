74ecfc3c25fdd19b6a919cfaf2e9c7512c779b1f
Zia Ur Rehman (Z.R.F) <ziaurr3hman@hotmail.com>
2017-04-08 00:38:48 +0500

    fixed theme and new feature added

--- FacebookAutoLikeProfessional.meta.js
+++ FacebookAutoLikeProfessional.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
-// @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeProfessional.user.js
-// @version  		23.0
+// @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeUnlimited.user.js
+// @version  		24.0
 // ==/UserScript==
