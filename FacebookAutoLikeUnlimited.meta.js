ca9d3ed8f9b6fbd3f80302b08809b60786eaeecf
Zia Ur Rehman (Z.R.F) <ziaurr3hman@hotmail.com>
2017-11-11 19:24:24 +0500

    Design and Bugs Fixed, Update 27.6

--- FacebookAutoLikeUnlimited.meta.js
+++ FacebookAutoLikeUnlimited.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
 // @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeUnlimited.user.js
-// @version  		27.5
+// @version  		27.6
 // ==/UserScript==
