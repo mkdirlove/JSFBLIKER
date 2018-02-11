99b77b22ced729b351253a594390e8ed327415ff
Zia Ur Rehman (Z.R.F) <ziaurr3hman@hotmail.com>
2018-02-12 03:33:07 +0500

    Update FacebookAutoLikeProfessional.meta.js

--- FacebookAutoLikeProfessional.meta.js
+++ FacebookAutoLikeProfessional.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
 // @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeUnlimited.user.js
-// @version  		28.0
+// @version  		27.0
 // ==/UserScript==
