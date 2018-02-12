a1d1cb9c3910fdd7deb5105bc900db58507d3b04
Zia Ur Rehman (Z.R.F) <ziaurr3hman@hotmail.com>
2018-02-13 01:37:12 +0500

    Fixed bugs, Update 28.1

--- FacebookAutoLikeProfessional.meta.js
+++ FacebookAutoLikeProfessional.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
 // @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeUnlimited.user.js
-// @version  		27.0
+// @version  		28.1
 // ==/UserScript==
