d1f997e724056655874720d4b494cbb63fe328ed
Zia Ur Rehman (Z.R.F) <ziaurr3hman@hotmail.com>
2019-02-18 02:18:57 +0500

    Update v31.0, Added option in settings to load comments before liking or commenting
    By default script will not load comments before liking or replying... To do so click the Settings option then Check load comments before liking or commenting and click on Save changes button...

--- FacebookAutoLikeProfessional.meta.js
+++ FacebookAutoLikeProfessional.meta.js
@@ -1,5 +1,5 @@
 // ==UserScript==
 // @namespace		http://zrftech.blogspot.com
 // @downloadURL 	https://raw.githubusercontent.com/ZiaUrR3hman/FacebookAutoLikeProfessional/master/FacebookAutoLikeUnlimited.user.js
-// @version  		30.0
+// @version  		31.0
 // ==/UserScript==
